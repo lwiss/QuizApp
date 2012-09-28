335e52957ed67c7b4d4e30a4d106f7572f60774c
3arbouch <mohamed.benarbia@epfl.ch>
2012-09-28 17:19:35 +0200

    Project updated

---
+++ Sweng2012QuizApp/src/epfl/sweng/sweng2012quizapp/MainActivity.java
@@ -0,0 +1,20 @@
+package epfl.sweng.sweng2012quizapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
