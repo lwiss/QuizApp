b2d8b8c87cf1cb9a5b82deae4ed1a3ddc0adae7d
3arbouch <mohamed.benarbia@epfl.ch>
2012-10-03 20:49:43 +0200

    Add Sweng2012QuizAppTest for testing with Junit Test ShowQuestionsActivityTest

---
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -0,0 +1,15 @@
+package epfl.sweng.test;
+
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ShowQuestionsActivityTest extends ActivityInstrumentationTestCase2<ShowQuestionsActivity > {
+
+	public ShowQuestionsActivityTest() {
+        super(ShowQuestionsActivity.class);
+    }
+
+    public void testHello() {
+        fail("Not yet implemented");
+    }
+}
