d1acb666ae01775ece18560b8b468889a227c3cd
3arbouch <mohamed.benarbia@epfl.ch>
2012-10-03 20:14:39 +0200

    first Step : Creating showQuestionsActivity

---
+++ Sweng2012QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -0,0 +1,23 @@
+package epfl.sweng.showquestions;
+
+import epfl.sweng.R;
+import epfl.sweng.R.layout;
+import epfl.sweng.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ShowQuestionsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
