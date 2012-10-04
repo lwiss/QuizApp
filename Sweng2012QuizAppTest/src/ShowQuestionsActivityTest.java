de5fcf51d7b73e2e9eb5f13e6f16283eb5e91488
3arbouch <mohamed.benarbia@epfl.ch>
2012-10-04 22:32:43 +0200

    compilation with ant

---
+++ Sweng2012QuizAppTest/src/ShowQuestionsActivityTest.java
@@ -0,0 +1,15 @@
+import epfl.sweng.showquestions.ShowQuestionsActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class ShowQuestionsActivityTest extends ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+
+	
+	public ShowQuestionsActivityTest() {
+        super(ShowQuestionsActivity.class);
+    }
+
+    public void testHello() {
+        fail("Not yet implemented");
+    }
+}
