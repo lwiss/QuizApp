3c6aa4f70bc5b8f2c8e21708ce993afafb837030
3arbouch <mohamed.benarbia@epfl.ch>
2012-10-04 23:33:42 +0200

    Connetion with the server established

---
+++ Sweng2012QuizApp/src/epfl/sweng/servercomm/SwengHttpClientFactory.java
@@ -0,0 +1,64 @@
+package epfl.sweng.servercomm;
+
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.RedirectHandler;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.AbstractHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+/**
+ * This factory creates HttpClients. It also allows to inject custom HttpClients
+ * for testing.
+ */
+public class SwengHttpClientFactory {
+
+	private static AbstractHttpClient httpClient;
+	private static final int HTTP_PORT = 80;
+	private final static int HTTPS_PORT = 443;
+	
+	
+	public static synchronized AbstractHttpClient getInstance() {
+		if (httpClient == null) {
+			httpClient = create();
+		}
+		
+		return httpClient;
+	}
+
+	public static synchronized void setInstance(AbstractHttpClient instance) {
+		httpClient = instance;
+	}
+
+	final private static RedirectHandler REDIRECT_NO_FOLLOW = new RedirectHandler() {
+		@Override
+		public boolean isRedirectRequested(HttpResponse response, HttpContext context) {
+			return false;
+		}
+
+		@Override
+		public URI getLocationURI(HttpResponse response, HttpContext context) throws org.apache.http.ProtocolException {
+			return null;
+		}
+	};
+
+	private static AbstractHttpClient create() {
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), HTTP_PORT));
+		schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), HTTPS_PORT));
+		HttpParams params = new BasicHttpParams();
+		ThreadSafeClientConnManager connManager = new ThreadSafeClientConnManager(params, schemeRegistry);
+		AbstractHttpClient result = new DefaultHttpClient(connManager, params);
+		result.setRedirectHandler(REDIRECT_NO_FOLLOW);
+		return result;
+	}
+
+}
--- Sweng2012QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
+++ Sweng2012QuizApp/src/epfl/sweng/showquestions/ShowQuestionsActivity.java
@@ -1,23 +1,101 @@
 package epfl.sweng.showquestions;
 
+import java.io.IOException;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import epfl.sweng.R;
 import epfl.sweng.R.layout;
 import epfl.sweng.R.menu;
+import epfl.sweng.servercomm.SwengHttpClientFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
 
 public class ShowQuestionsActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_questions);
-    }
+	private final String URL = "https://sweng-quiz.appspot.com/quizquestions/random";
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_show_questions, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_questions);
+
+		// check the Network Connection
+		ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo netInfo = conMan.getActiveNetworkInfo();
+
+		if (netInfo != null && netInfo.isConnected()) {
+			System.out.println("il y a une conexion !");
+			new DownloadJSONObject().execute(URL);
+		}
+
+	}
+
+	private JSONObject downloadContent(String urlServer) throws JSONException {
+		HttpGet httpget = new HttpGet(urlServer);
+		ResponseHandler<String> handler = new BasicResponseHandler();
+		String question = "";
+		try {
+			question = SwengHttpClientFactory.getInstance().execute(httpget,
+					handler);
+		} catch (IOException e) {
+			System.err.println("Enable to connect to the Server!");
+		}
+
+		return new JSONObject(question);
+
+	}
+
+	private class DownloadJSONObject extends
+			AsyncTask<String, String, JSONObject> {
+
+		@Override
+		protected JSONObject doInBackground(String... urls) {
+			// TODO Auto-generated method stub
+			try {
+				return downloadContent(urls[0]);
+
+			} catch (JSONException e) {
+				System.err.println("ERROR! NULL JSONOBJECT");
+				return null;
+			}
+
+		}
+
+		@Override
+		protected void onPostExecute(JSONObject jsonObject) {
+			try {
+
+				String quesion = jsonObject.getString("question");
+				int solutionIndex = jsonObject.getInt("solutionIndex");
+				JSONArray answersArray = jsonObject.getJSONArray("answers");
+				System.out.println("la question est : " + quesion);
+				System.out.println("les reponses sont " + answersArray);
+				System.out.println("L'index de la solution est "
+						+ solutionIndex);
+
+			} catch (JSONException e) {
+				System.err.println("ERREUR Lors de l'extraction des données");
+			}
+
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_show_questions, menu);
+		return true;
+	}
+
 }
