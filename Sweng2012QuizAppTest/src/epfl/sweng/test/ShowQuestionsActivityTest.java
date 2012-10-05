a56cf1a849f663eb47031ee936d31f6f9f6da32a
bhyassine <yassine.benhamadi@epfl.ch>
2012-10-05 19:00:12 +0200

    final

--- Sweng2012QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/ShowQuestionsActivityTest.java
@@ -8,58 +8,67 @@ import epfl.sweng.showquestions.ShowQuestionsActivity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 
-public class ShowQuestionsActivityTest extends ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
-    private Solo solo;
-    String question=(String) ShowQuestionsActivity.getQuestion().getText();
-    String answers[]=ShowQuestionsActivity.getAnswers();
-    int solution=ShowQuestionsActivity.getSolution();
+/**
+ * 
+ * @author Eagles
+ * 
+ */
+public class ShowQuestionsActivityTest extends
+		ActivityInstrumentationTestCase2<ShowQuestionsActivity> {
+	private Solo solo;
+	private String question = (String) ShowQuestionsActivity.getQuestion()
+			.getText();
+	private String [] answers = ShowQuestionsActivity.getAnswers();
+	private int solution = ShowQuestionsActivity.getSolution();
 
-    public ShowQuestionsActivityTest() {
-        super(ShowQuestionsActivity.class);
-    }
+	public ShowQuestionsActivityTest() {
+		super(ShowQuestionsActivity.class);
+	}
 
-    @Override
-    protected void setUp() throws Exception {
-        solo = new Solo(getInstrumentation(), getActivity());
-    }
+	@Override
+	protected void setUp() throws Exception {
+		solo = new Solo(getInstrumentation(), getActivity());
+	}
 
-    public void testShowQuestion() throws JSONException
-    {
-    	assertTrue("Question is displayed", solo.searchText(question));
-        for (int i=0; i < answers.length; i++)
-        {
-        	assertTrue("Answer "+i+" is displayed", solo.searchText(answers[i]));
-        }
-        Button nextQuestionButton = solo.getButton("Next question");
-        assertFalse("Next question button is disabled", nextQuestionButton.isEnabled());
-    }
-    public void testFalseResponses()
-    {
-        for (int i=0; i < answers.length; i++)
-        {
-        	if (i!=solution)
-        	{
-        		solo.clickOnText(answers[i]);
-        		assertTrue("False answer "+i+" showen as so",solo.searchText(answers[i]+" \u2718"));
-        		Button nextQuestionButton = solo.getButton("Next question");
-        		assertFalse("Next question button is disabled", nextQuestionButton.isEnabled());
-        	}
-        }
-    }
-    public void testTrueResponse()
-    {
-    	solo.clickOnText(answers[solution]);
-    	assertTrue("True answer "+solution+" showen as so",solo.searchText(answers[solution]+" \u2714"));
-    	Button nextQuestionButton = solo.getButton("Next question");
-    	assertTrue("Next question button is enabled", nextQuestionButton.isEnabled());
-    }
-    public void testNoMoreEffects()
-    {
-        for (int i=0; i < answers.length; i++)
-        {
-        	solo.clickOnText(answers[i]);
-        	Button nextQuestionButton = solo.getButton("Next question");
-        	assertTrue("Next question button is still enabled", nextQuestionButton.isEnabled());
-        }
-    }
+	public void testShowQuestion() throws JSONException {
+		assertTrue("Question is displayed", solo.searchText(question));
+		for (int i = 0; i < answers.length; i++) {
+			assertTrue("Answer " + i + " is displayed",
+					solo.searchText(answers[i]));
+		}
+		Button nextQuestionButton = solo.getButton("Next question");
+		assertFalse("Next question button is disabled",
+				nextQuestionButton.isEnabled());
+	}
+
+	public void testFalseResponses() {
+		for (int i = 0; i < answers.length; i++) {
+			if (i != solution) {
+				solo.clickOnText(answers[i]);
+				assertTrue("False answer " + i + " showen as so",
+						solo.searchText(answers[i] + " \u2718"));
+				Button nextQuestionButton = solo.getButton("Next question");
+				assertFalse("Next question button is disabled",
+						nextQuestionButton.isEnabled());
+			}
+		}
+	}
+
+	public void testTrueResponse() {
+		solo.clickOnText(answers[solution]);
+		assertTrue("True answer " + solution + " showen as so",
+				solo.searchText(answers[solution] + " \u2714"));
+		Button nextQuestionButton = solo.getButton("Next question");
+		assertTrue("Next question button is enabled",
+				nextQuestionButton.isEnabled());
+	}
+
+	public void testNoMoreEffects() {
+		for (int i = 0; i < answers.length; i++) {
+			solo.clickOnText(answers[i]);
+			Button nextQuestionButton = solo.getButton("Next question");
+			assertTrue("Next question button is still enabled",
+					nextQuestionButton.isEnabled());
+		}
+	}
 }
