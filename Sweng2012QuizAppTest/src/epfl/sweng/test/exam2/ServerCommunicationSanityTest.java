74b7ecd95ef2c8419634537aa86d2aed8ea29358
3arbouch <mohamed.benarbia@epfl.ch>
2012-11-16 18:55:19 +0100

    Adding exam2

--- Sweng2012QuizAppTest/src/epfl/sweng/test/exam2/ServerCommunicationSanityTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/exam2/ServerCommunicationSanityTest.java
@@ -8,7 +8,6 @@ import org.apache.http.HttpException;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHttpResponse;
@@ -21,15 +20,15 @@ import epfl.sweng.servercomm.SwengHttpClientFactory;
 import epfl.sweng.servercomm.search.CommunicationException;
 import epfl.sweng.servercomm.search.QuestionSearchCommunication;
 import epfl.sweng.servercomm.search.QuestionSearchCommunicationFactory;
-import epfl.sweng.test.ShowQuestionsActivityTest.MockHttpRequestExecutor;
 import android.test.AndroidTestCase;
+
 /**
  * 
  * @author crazybhy
- *
+ * 
  */
 public class ServerCommunicationSanityTest extends AndroidTestCase {
-	
+
 	private QuestionSearchCommunication questionSearch;
 
 	@Override
@@ -38,75 +37,69 @@ public class ServerCommunicationSanityTest extends AndroidTestCase {
 		SwengHttpClientFactory.setInstance(new MockHttpClient());
 		questionSearch = QuestionSearchCommunicationFactory.getInstance();
 	}
-	
+
 	public void testInterfaceImplementation() {
 		String className = questionSearch.getClass().getName();
-		assertEquals("epfl.sweng.servercomm.search.DefaultQuestionSearchCommunication",
+		assertEquals(
+				"epfl.sweng.servercomm.search.DefaultQuestionSearchCommunication",
 				className);
 	}
-	
+
 	public void testSearchByOwner() throws CommunicationException {
-		List<QuizQuestion> questions = 
-				questionSearch.getQuestionsByOwner("joe");
-		assertTrue(questions.size()==2);
+		List<QuizQuestion> questions = questionSearch
+				.getQuestionsByOwner("joe");
+		assertTrue(questions.size() == 2);
 	}
-	
+
 	public void testSearchByTag() throws CommunicationException {
-		List<QuizQuestion> questions =
-				questionSearch.getQuestionsByTag("tag");
-		assertTrue(questions.size()==2);
+		List<QuizQuestion> questions = questionSearch.getQuestionsByTag("tag");
+		assertTrue(questions.size() == 2);
 	}
-	
+
 	public void testSearchByInvalidOwner() throws CommunicationException {
-		List<QuizQuestion> questions;
+
 		try {
-			questions = 
-					questionSearch.getQuestionsByOwner("j o e");
+			questionSearch.getQuestionsByOwner("j o e");
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			assertTrue(e.getLocalizedMessage().equals("Invalid Characters"));
 		}
 		try {
-			questions = 
-					questionSearch.getQuestionsByOwner("");
+			questionSearch.getQuestionsByOwner("");
 		} catch (IllegalArgumentException e) {
 			assertTrue(e.getLocalizedMessage().equals("Too long or Empty"));
 		}
 		try {
-			questions = 
-					questionSearch.getQuestionsByOwner("abdcdefghijklmnopqrstuvwxyz");
+			questionSearch.getQuestionsByOwner("abdcdefghijklmnopqrstuvwxyz");
 		} catch (IllegalArgumentException e) {
 			assertTrue(e.getLocalizedMessage().equals("Too long or Empty"));
 		}
 	}
-	
+
 	public void testSearchByInvalidTag() throws CommunicationException {
-		List<QuizQuestion> questions;
+
 		try {
-			questions = 
-					questionSearch.getQuestionsByTag("t a g");
+			questionSearch.getQuestionsByTag("t a g");
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			assertTrue(e.getLocalizedMessage().equals("Invalid Characters"));
 		}
 		try {
-			questions = 
-					questionSearch.getQuestionsByOwner("");
+			questionSearch.getQuestionsByOwner("");
 		} catch (IllegalArgumentException e) {
 			assertTrue(e.getLocalizedMessage().equals("Too long or Empty"));
 		}
 		try {
-			questions = 
-					questionSearch.getQuestionsByOwner("abdcdefghijklmnopqrstuvwxyz");
+			questionSearch.getQuestionsByOwner("abdcdefghijklmnopqrstuvwxyz");
 		} catch (IllegalArgumentException e) {
 			assertTrue(e.getLocalizedMessage().equals("Too long or Empty"));
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @author crazybhy
-	 *
+	 * 
 	 */
 	public class MockHttpClient extends DefaultHttpClient {
 		@Override
@@ -126,28 +119,27 @@ public class ServerCommunicationSanityTest extends AndroidTestCase {
 				final HttpClientConnection conn, final HttpContext context)
 			throws IOException, HttpException {
 			final int statusOk = 200;
-			final int statusNotFound = 404;
-			final int statusError = 303;
+
 			HttpResponse response = new BasicHttpResponse(new BasicStatusLine(
 					HttpVersion.HTTP_1_1, statusOk, "OK"));
 			response.setHeader("Content-type", "application/json;charset=utf-8");
-			response.setEntity(new StringEntity("[{"
-					+ " \"question\": \"What is the answer to life, the universe and everything?\","
-					+ " \"answers\": [ \"42\", \"24\" ],"
-					+ " \"solutionIndex\": 0,"
-					+ " \"id\": 4243,"
-					+ " \"owner\": \"joe\","
-					+ " \"tags\": [ \"tag\"]"
-					+ " },"
-					+ "{"
-					+ " \"question\": \"What is NOT the answer to life, the universe and everything?\","
-					+ " \"answers\": [ \"42\", \"24\" ],"
-					+ " \"solutionIndex\": 1,"
-					+ " \"id\": 4342,"
-					+ " \"owner\": \"joe\","
-					+ " \"tags\": [ \"tag\"]" + " }]"));
+			response.setEntity(new StringEntity(
+					"[{"
+							+ " \"question\": \"What is the answer to life, the universe and everything?\","
+							+ " \"answers\": [ \"42\", \"24\" ],"
+							+ " \"solutionIndex\": 0,"
+							+ " \"id\": 4243,"
+							+ " \"owner\": \"joe\","
+							+ " \"tags\": [ \"tag\"]"
+							+ " },"
+							+ "{"
+							+ " \"question\": \"What is NOT the answer to life, the universe and everything?\","
+							+ " \"answers\": [ \"42\", \"24\" ],"
+							+ " \"solutionIndex\": 1," + " \"id\": 4342,"
+							+ " \"owner\": \"joe\"," + " \"tags\": [ \"tag\"]"
+							+ " }]"));
 			return response;
 		}
 	}
-	
+
 }
