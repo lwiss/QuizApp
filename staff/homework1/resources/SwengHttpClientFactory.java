9a417da64026b7eb1b44dd0e095a60582f2d4767
SwEng Staff <sweng-staff@googlegroups.com>
2012-11-18 16:53:29 +0100

    Pushed Homework 5.

--- staff/homework1/resources/SwengHttpClientFactory.java
+++ staff/homework1/resources/SwengHttpClientFactory.java
@@ -1,13 +1,18 @@
 package epfl.sweng.servercomm;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.RedirectHandler;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.HttpResponse;
 import org.apache.http.impl.client.AbstractHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
@@ -24,13 +29,13 @@ public class SwengHttpClientFactory {
 	private static AbstractHttpClient httpClient;
 	private static final int HTTP_PORT = 80;
 	private final static int HTTPS_PORT = 443;
-	
-	
+
+
 	public static synchronized AbstractHttpClient getInstance() {
 		if (httpClient == null) {
 			httpClient = create();
 		}
-		
+
 		return httpClient;
 	}
 
@@ -50,6 +55,26 @@ public class SwengHttpClientFactory {
 		}
 	};
 
+	final private static CookieStore COOKIE_MONSTER = new CookieStore() {
+		@Override
+		public void addCookie(Cookie cookie) {
+		}
+
+		@Override
+		public void clear() {
+		}
+
+		@Override
+		public boolean clearExpired(Date date) {
+			return true;
+		}
+
+		@Override
+		public List<Cookie> getCookies() {
+			return new ArrayList<Cookie>();
+		}
+	};
+
 	private static AbstractHttpClient create() {
 		SchemeRegistry schemeRegistry = new SchemeRegistry();
 		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), HTTP_PORT));
@@ -58,6 +83,7 @@ public class SwengHttpClientFactory {
 		ThreadSafeClientConnManager connManager = new ThreadSafeClientConnManager(params, schemeRegistry);
 		AbstractHttpClient result = new DefaultHttpClient(connManager, params);
 		result.setRedirectHandler(REDIRECT_NO_FOLLOW);
+		result.setCookieStore(COOKIE_MONSTER);
 		return result;
 	}
 
