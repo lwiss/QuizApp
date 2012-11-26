bdf8837f76d29da543de5479add2dd0758f6dbd8
bhyassine <yassine.benhamadi@epfl.ch>
2012-11-26 18:12:07 +0100

    Reinstalling

--- staff/homework1/resources/MockHttpClient.java
+++
@@ -1,9 +0,0 @@
-/** To use this, call SwengHttpClientFactory.setInstance(new MockHttpClient())
- * in your testing code. Remember that the app always has to use
- * SwengHttpClientFactory.getInstance() if it needs an HttpClient. */
-public class MockHttpClient extends DefaultHttpClient {
-    @Override
-    protected HttpRequestExecutor createRequestExecutor() {
-        return new MockHttpRequestExecutor();
-    }
-}
