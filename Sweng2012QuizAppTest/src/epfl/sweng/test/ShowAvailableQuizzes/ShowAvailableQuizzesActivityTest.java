e4207569ecb5fcb9af7fcdf4f3d290144887f6a3
bhyassine <yassine.benhamadi@epfl.ch>
2012-11-30 13:33:33 +0100

    Fixing Test

--- Sweng2012QuizAppTest/src/epfl/sweng/test/ShowAvailableQuizzes/ShowAvailableQuizzesActivityTest.java
+++ Sweng2012QuizAppTest/src/epfl/sweng/test/ShowAvailableQuizzes/ShowAvailableQuizzesActivityTest.java
@@ -2,7 +2,6 @@ package epfl.sweng.test.ShowAvailableQuizzes;
 
 import com.jayway.android.robotium.solo.Solo;
 
-import epfl.sweng.entry.MainActivity;
 import epfl.sweng.quizzes.ShowAvailableQuizzesActivity;
 import epfl.sweng.servercomm.SwengHttpClientFactory;
 import android.test.ActivityInstrumentationTestCase2;
@@ -14,13 +13,13 @@ import android.test.ActivityInstrumentationTestCase2;
  * 
  */
 public class ShowAvailableQuizzesActivityTest extends
-		ActivityInstrumentationTestCase2<MainActivity> {
+		ActivityInstrumentationTestCase2<ShowAvailableQuizzesActivity> {
 	private Solo solo;
 	private static final String NO_QUIZZES_AVAILABLE = "There are no quizzes available at the moment.";
 	private static final String ERROR_COMMUNICATION = "An error occurred while fetching quizzes.";
 
 	public ShowAvailableQuizzesActivityTest() {
-		super(MainActivity.class);
+		super(ShowAvailableQuizzesActivity.class);
 	}
 
 	@Override
@@ -29,18 +28,8 @@ public class ShowAvailableQuizzesActivityTest extends
 
 	}
 
-	public void testTakequizzbutton() {
-		assertTrue("Take a Quiz button", solo.searchButton("Take a Quiz"));
-		solo.clickOnButton("Take a Quiz");
-		solo.assertCurrentActivity("Show available quizzes activity",
-				ShowAvailableQuizzesActivity.class);
-		solo.goBack();
-	}
-
 	public void testAllQuizzezTitlesAvailable() {
 		SwengHttpClientFactory.setInstance(new MockValidQuizzesTitle());
-		solo.assertCurrentActivity("Main activity", MainActivity.class);
-		solo.clickOnButton("Take a Quiz");
 		solo.searchText("easy quizz");
 		solo.searchText("hard quizz");
 		solo.goBack();
@@ -48,8 +37,6 @@ public class ShowAvailableQuizzesActivityTest extends
 
 	public void testNoQuizzAvailableMesaage() {
 		SwengHttpClientFactory.setInstance(new MockNoquizzesAvailable());
-		solo.assertCurrentActivity("Main activity", MainActivity.class);
-		solo.clickOnButton("Take a Quiz");
 		assertTrue("Search about No quizzes available text ",
 				solo.searchText(NO_QUIZZES_AVAILABLE));
 		solo.goBack();
@@ -58,7 +45,6 @@ public class ShowAvailableQuizzesActivityTest extends
 
 	public void testErrorCommunicationMessage() throws Exception {
 		SwengHttpClientFactory.setInstance(new MockErrorCommunication());
-		solo.clickOnButton("Take a Quiz");
 		assertTrue("Search about Error Communication Text ",
 				solo.searchText(ERROR_COMMUNICATION));
 		solo.goBack();
@@ -67,8 +53,6 @@ public class ShowAvailableQuizzesActivityTest extends
 
 	public void testMalformedJson() {
 		SwengHttpClientFactory.setInstance(new MockMalformedJsonResponse());
-		solo.assertCurrentActivity("Main activity", MainActivity.class);
-		solo.clickOnButton("Take a Quiz");
 		assertTrue(!solo.searchText("invalid id"));
 		solo.goBack();
 
